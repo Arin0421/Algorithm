7e17e302299ef6a3f2f7eb35c5daedf0faf3613b
Arin0421 <73944423+Arin0421@users.noreply.github.com>
2021-08-05 17:30:41 +0900

    Create BOJ 7568_덩치.py

---
+++ Bruteforcing/BOJ 7568_덩치.py
@@ -0,0 +1,13 @@
+student_num=int(input())
+student=[]
+
+for _ in range(student_num):
+    weight,height=map(int,input().split())
+    student.append((weight,height))
+
+for i in student:
+    rank=1
+    for j in student:
+        if i[0]<j[0] and i[1]<j[1]:
+            rank+=1
+    print(rank,end=" ")
