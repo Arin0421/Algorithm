1b9f1e2221a47fd1c1898d14a9a46fa13e3b865b
Arin0421 <73944423+Arin0421@users.noreply.github.com>
2022-01-19 00:44:48 +0900

    Update BOJ_1373_2진수 8진수.py

--- Mathematics/BOJ_1373_2진수 8진수.py
+++ Mathematics/BOJ_1373_2진수 8진수.py
@@ -1,4 +1,17 @@
-a=input()
-n=int(a,2)
+##
+##
+##n=input()
+##
+##s=int(n,2)
+##
+##answer=''
+##data="012345678"
+##
+##while s!=0:
+##    answer+=str(data[s%8])
+##    s//=8
+##
+##print(answer[::-1])
 
-print(n)
+
+print(oct(int(input(),2))[2:])
