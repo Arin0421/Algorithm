7e4a11bfbe1877f8fd0db3cbd75437a0c8606c86
Arin0421 <73944423+Arin0421@users.noreply.github.com>
2022-01-20 21:23:56 +0900

    Create BOJ_2089_-2진수.py

---
+++ Mathematics/BOJ_2089_-2진수.py
@@ -0,0 +1,14 @@
+n=int(input())
+result=''
+
+while n!=0:
+    div = n//-2
+    if n%-2<0:
+        div+=1
+    result+=str(n-(div*-2))
+    n=div
+
+if result:
+    print(result[::-1])
+else:
+    print(0)
