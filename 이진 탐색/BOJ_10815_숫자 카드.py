e4be153160c2d7ec21cf3c9ae380a2df9d14b4da
Arin0421 <arin9804@naver.com>
2022-02-10 14:57:10 +0900

    Update BOJ_10815_숫자 카드.py

--- 이진 탐색/BOJ_10815_숫자 카드.py
+++ 이진 탐색/BOJ_10815_숫자 카드.py
@@ -1,24 +1,31 @@
-from sys import stdin
-n=stdin.readline().rstrip()
-array=list(map(int,stdin.readline().split()))
-array.sort()
-m=stdin.readline().rstrip()
-target=list(map(int,stdin.readline().split()))
+import sys
+input=sys.stdin.readline
 
-def binary(array, target, start, end):
-    if start > end:
-        return 0
-
-    mid = (start + end) //2
-
-    if array[mid] == target:
-        return 1
-    elif array[mid] > target:
-        return binary(array, target, start, mid-1)
+def binary_search(array, target, start, end):
+    if start>end:
+        return None
+    mid=(start+end)//2
+    if array[mid]==target:
+        return mid
+    elif array[mid]>target:
+        return binary_search(array, target, start,mid-1)
     else:
-        return binary(array, target, mid+1, end)
+        return binary_search(array, target, mid+1, end)
 
-for num in target:
-    start=0
-    end=len(array)-1
-    print(binary(array, num, start, end), end=' ')
+n=int(input())
+n_list=list(map(int,input().split()))
+n_list.sort()
+
+m=int(input())
+m_list=list(map(int,input().split()))
+
+
+for x in m_list:
+    result=binary_search(n_list,x,0,n-1)
+    if result==None:
+        print("0",end=" ")
+    else:
+        print("1",end=" ")
+
+
+
