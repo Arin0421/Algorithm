e4e8316d0be6315c1f931bba3c49a8dbebe909f0
Arin0421 <arin9804@naver.com>
2022-02-14 23:12:10 +0900

    Update BOJ_2805_나무 자르기.py

--- 이진 탐색/BOJ_2805_나무 자르기.py
+++ 이진 탐색/BOJ_2805_나무 자르기.py
@@ -1,26 +1,25 @@
 import sys
-n, m = map(int,sys.stdin.readline().split())
-array = list(map(int,input().split()))
+k,n=map(int,input().split())
+array=list(map(int,input().split()))
 
-start = 0
-end = max(array)
+start=1
+end=max(array)
 
 result=0
-
-while start <= end :
-    mid = (start + end) //2
-    total = 0
-
+while(start<=end):
+    total=0
+    mid=(start+end)//2
+    
     for x in array:
-        if x >= mid:
-            total += x-mid
-            if total > m:
-                break
-
-    if total >= m:
-        result = mid
-        start = mid +1
+        if x>=mid:
+            total+=x-mid
+            
+    if total>=n:
+        result=mid
+        start=mid+1
+        
     else:
-        end = mid -1
-
+        end=mid-1
+        
 print(result)
+    
