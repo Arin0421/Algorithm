1f6beb6471761a4bee0305da4ded4accfeaea527
Arin0421 <arin9804@naver.com>
2022-05-20 17:13:57 +0900

    Update 2021 KAKAO BLIND RECRUITMENT_신규 아이디 추천.py

--- 시뮬레이션과 구현/2021 KAKAO BLIND RECRUITMENT_신규 아이디 추천.py
+++ 시뮬레이션과 구현/2021 KAKAO BLIND RECRUITMENT_신규 아이디 추천.py
@@ -13,3 +13,26 @@ for i in s:
 #3
 while '..' in answer:
     answer = answer.replace("..",".")
+
+#4
+if answer and answer[0] == '.':
+    answer = answer[1:]
+if answer and answer[-1] == '.':
+    answer = answer[:-1]
+
+#5
+if answer=='':
+    answer='a'
+
+#6
+if len(answer)>15:
+    answer = answer[0:15]
+    if answer[-1]==".":
+        answer=answer[:-1]
+
+#7
+while len(answer) <3:
+    answer+=answer[-1]
+
+    
+print(answer)
