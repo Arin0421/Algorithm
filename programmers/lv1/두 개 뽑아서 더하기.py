ff01c400973cd7ab107d1ada9a93e2f89e861aa9
Arin0421 <arin9804@naver.com>
2022-06-02 18:26:57 +0900

    Create 두 개 뽑아서 더하기.py

---
+++ programmers/lv1/두 개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = []
+    result=0
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            result=numbers[i]+numbers[j]
+            if result not in answer:
+                answer.append(result)
+    answer.sort()
+    return answer
