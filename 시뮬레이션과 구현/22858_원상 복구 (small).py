853301493abf034d3a9ea42373ffe20135be278e
Arin Lee <73944423+Arin0421@users.noreply.github.com>
2023-01-19 23:23:58 +0900

    Create 22858_원상 복구 (small).py

---
+++ 시뮬레이션과 구현/22858_원상 복구 (small).py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n,k=map(int,input().split())
+
+s=list(map(int,input().split()))
+
+d=list(map(int,input().split()))
+
+for _ in range(k):
+    p=[0]*n
+    for i in range(n):
+        p[d[i]-1]=s[i]
+    s=p
+
+print(*s)
