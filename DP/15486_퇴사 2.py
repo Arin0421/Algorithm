1fb17177c54bcaf9707890f42dfde5b7a8d11dc8
Arin Lee <73944423+Arin0421@users.noreply.github.com>
2023-02-01 23:49:41 +0900

    Create 15486_퇴사 2.py

---
+++ DP/15486_퇴사 2.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+t=[]
+p=[]
+dp=[0]*(n+1)
+
+for  _ in range(n):
+    x,y=map(int,input().split())
+    t.append(x)
+    p.append(y)
+    
+max_result=0
+for i in range(n-1,-1,-1):
+    time = i+t[i]
+    if time<=n:
+        dp[i]=max(dp[time]+p[i],max_result)
+        max_result=dp[i]
+    else:
+        dp[i]=max_result
+
+
+print(max(dp))
