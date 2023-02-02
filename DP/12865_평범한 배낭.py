dd346b5db6a79505b6d866e3f688e8495e80bd32
Arin Lee <73944423+Arin0421@users.noreply.github.com>
2023-02-03 01:03:31 +0900

    Create 12865_평범한 배낭.py

---
+++ DP/12865_평범한 배낭.py
@@ -0,0 +1,8 @@
+n,k=map(int,input().split())
+backpack=[list(map(int,input().split())) for _ in range(n)]
+dp=[0]*(k+1)
+
+for w,v in backpack:
+    for i in range(k,w-1,-1):
+        dp[i]=max(dp[i],dp[i-w]+v)
+print(dp[-1])
