403776611dffd8039d95af9c10b874949c86164e
Arin Lee <73944423+Arin0421@users.noreply.github.com>
2023-02-03 14:09:27 +0900

    Create 2293_동전 1.py

---
+++ DP/2293_동전 1.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+n,k=map(int,input().split())
+c=[]
+for i in range(n):
+    c.append(int(input()))
+
+dp=[0]*(k+1)
+dp[0]=1
+for i in c:
+    for j in range(i,k+1):
+        if j-i>=0:
+            dp[j]+=dp[j-i]
+
+print(dp[-1])
