5489fbea3ad8d1e1e9c8db768cadc4370b0bcb44
Arin Lee <73944423+Arin0421@users.noreply.github.com>
2023-02-11 18:45:22 +0900

    Create 괄호 변환.py
    프로그래머스

---
+++ DFS,BFS/괄호 변환.py
@@ -0,0 +1,44 @@
+def get_index(p):
+    cnt=0
+    for i in range(len(p)):
+        if p[i]=='(':
+            cnt+=1
+        else:
+            cnt-=1
+        if cnt==0:
+            return i
+        
+def check_proper(p):
+    cnt=0
+    for i in p:
+        if i=='(':
+            cnt+=1
+        else:
+            if cnt==0:
+                return False
+            cnt-=1
+    return True
+
+
+def solution(p):
+    answer = ''
+    if p=='':
+        return answer
+    idx=get_index(p)
+    u=p[:idx+1]
+    v=p[idx+1:]
+    
+    if check_proper(u):
+        answer=u+solution(v)
+    else:
+        answer='('
+        answer+=solution(v)
+        answer+=')'
+        u=list(u[1:-1])
+        for i in range(len(u)):
+            if u[i]=='(':
+                u[i]=')'
+            else:
+                u[i]='('
+        answer+="".join(u)
+    return answer
