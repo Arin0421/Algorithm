4abae414c20e681c72bc590346b10f0e656fdaf3
Arin Lee <73944423+Arin0421@users.noreply.github.com>
2023-02-21 14:14:49 +0900

    second solution

--- greedy/큰 수의 법칙.py
+++ greedy/큰 수의 법칙.py
@@ -1,23 +1,15 @@
-#N,M,k를 공백으로 구분하여 입력받기
-n,m,k=map(int, input().split())
-#n개의 수를 공백으로 구분하여 입력받기
-data = list(map(int, inpuut().split()))
+n,m,k=map(int,input().split())
+arr=list(map(int,input().split()))
 
-data.sort()
-first = data[n-1]
-second = data[n-2]
+arr.sort(reverse=True)
 
-result = 0
-
-while True:
-    for i in range(k):#가장 큰 수를 k번 더하기
-        if m==0:
-            break
-        result += first
-        m-=1
-    if m==0:
-        break
-    result+=second
-    m-=1
-
+result=0
+cnt=0
+for _ in range(m):
+    if cnt==k:
+        result+=arr[1]
+        cnt=0
+    else:
+        result+=arr[0]
+        cnt+=1
 print(result)
