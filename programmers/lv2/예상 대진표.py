37b74c84e572882920efa8812e8a21e78f0313c9
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-02-24 21:12:09 +0900

    second solution

--- programmers/lv2/예상 대진표.py
+++ programmers/lv2/예상 대진표.py
@@ -1,5 +1,11 @@
+from math import ceil
 def solution(n,a,b):
-    answer = 0
-    while a!=b:
-        a,b=(a+1)//2,(b+1)//2
+    answer = 1
+    
+    for i in range(n):
+        if abs(a-b)==1 and max(a,b)%2==0:
+            break
+        a=ceil(a/2)
+        b=ceil(b/2)
+        answer+=1
     return answer
