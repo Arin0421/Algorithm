234ce2ed31537594465175706f726932df152270
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-04-12 17:25:40 +0900

    third solution

--- greedy/큰 수의 법칙.py
+++ greedy/큰 수의 법칙.py
@@ -1,15 +1,16 @@
 n,m,k=map(int,input().split())
-arr=list(map(int,input().split()))
+data=list(map(int,input().split()))
 
-arr.sort(reverse=True)
-
-result=0
+data.sort()
+ans=0
 cnt=0
+
 for _ in range(m):
     if cnt==k:
-        result+=arr[1]
+        ans+=data[-2]
         cnt=0
-    else:
-        result+=arr[0]
-        cnt+=1
-print(result)
+        continue
+    ans+=data[-1]
+    cnt+=1
+
+print(ans)
