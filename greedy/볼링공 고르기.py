e69158d0b74356288d6aa045b3fafcfc94cdb831
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-04-17 17:03:03 +0900

    Update 볼링공 고르기.py
    second solution

--- greedy/볼링공 고르기.py
+++ greedy/볼링공 고르기.py
@@ -1,17 +1,13 @@
+from itertools import combinations
+
 n,m=map(int,input().split())
 data=list(map(int,input().split()))
 
-#1부터 10까지의 무게를 담는 리스트
-array = [0]*11
+arr=list(combinations(data,2))
+ans=len(arr)
 
-for x in data:
-    #각 무게에 해당하는 볼링공의 개수
-    array[x]+=1
+for i,j in arr:
+    if i==j:
+        ans-=1
 
-result=0
-#1부터 m까지의 각 무게에 대해 처리
-for i in range(1,m+1):
-    n-=array[i] #A가 선택할 수 있는 볼링공의 수 제외
-    result+=array[i]*n
-
-print(result)
+print(ans)
