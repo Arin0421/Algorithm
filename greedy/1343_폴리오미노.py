e56c47719e9c8c62ab91e991ca9cb023695f266f
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-05-02 15:21:23 +0900

    Update 1343_폴리오미노.py

--- greedy/1343_폴리오미노.py
+++ greedy/1343_폴리오미노.py
@@ -1,12 +1,7 @@
 board=input()
 
-for i in range(0,len(board)-3):
-    if board[i:i+4]=='XXXX':
-        board=board.replace('XXXX','AAAA')
-
-for i in range(0,len(board)-1):
-    if board[i:i+2]=='XX':
-        board=board.replace('XX','BB')
+board = board.replace('XXXX','AAAA')
+board = board.replace('XX','BB')
 
 if 'X' in board:
     print(-1)
