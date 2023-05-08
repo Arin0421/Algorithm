f5243914b6884b039d47e1f220888730662d8724
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-05-08 17:51:19 +0900

    Create 15655_N과 M (6).py

---
+++ 백트래킹/15655_N과 M (6).py
@@ -0,0 +1,16 @@
+n,m=map(int,input().split())
+data=list(map(int,input().split()))
+data.sort()
+s=[]
+
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    for i in data:
+        if len(s)==0 or s[-1]<i:
+            s.append(i)
+            dfs()
+            s.pop()
+
+dfs()
