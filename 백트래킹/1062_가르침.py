0a87f3a4cfbc3585e632f8ea18814c6abf31d423
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-05-15 22:19:50 +0900

    Update 1062_가르침.py

--- 백트래킹/1062_가르침.py
+++ 백트래킹/1062_가르침.py
@@ -2,11 +2,9 @@ import sys
 
 n, k = map(int, input().split())
 
-# k 가 5보다 작으면 어떤 언어도 배울 수 없음
 if k < 5:
     print(0)
     exit()
-# k 가 26이면 모든 언어를 배울 수 있음
 elif k == 26:
     print(n)
     exit()
@@ -16,7 +14,6 @@ words = [(sys.stdin.readline().rstrip()) for _ in range(n)]
 
 learn = [0] * 26
 
-# 적어도 언어 하나는 배우기위해 a,c,i,n,t 는 무조건 배워야함
 for c in ('a', 'c', 'i', 'n', 't'):
     learn[ord(c) - ord('a')] = 1
 
