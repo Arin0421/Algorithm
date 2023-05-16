21fec12dd32382116190cbb9803d55a7012964ee
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-05-16 17:29:57 +0900

    Update BOJ_2178_미로 탐색.py

--- DFS,BFS/BOJ_2178_미로 탐색.py
+++ DFS,BFS/BOJ_2178_미로 탐색.py
@@ -1,26 +1,27 @@
 from collections import deque
-
-n,m=map(int,input().split())
-graph=[]
-for i in range(n):
-    graph.append(list(map(int,input())))
-queue=deque([])
+import sys
+input=sys.stdin.readline
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 def bfs(x,y):
-    queue.append((x,y))
-    while queue:
-        x,y=queue.popleft()
+    q=deque()
+    q.append((x,y))
+    while q:
+        x,y=q.popleft()
         for i in range(4):
-            nx=dx[i]+x
-            ny=dy[i]+y
+            nx=x+dx[i]
+            ny=y+dy[i]
             if 0<=nx<n and 0<=ny<m:
                 if graph[nx][ny]==1:
+                    q.append((nx,ny))
                     graph[nx][ny]=graph[x][y]+1
-                    queue.append([nx,ny])
     return graph[n-1][m-1]
-            
+
+n,m=map(int,input().split())
+graph=[]
+for _ in range(n):
+    graph.append(list(map(int,input().rstrip())))
 
 print(bfs(0,0))
