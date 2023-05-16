6522ed09c8b6dad1eda095c75a9a8889b2960123
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-05-16 17:40:38 +0900

    Update BOJ_7576_토마토.py

--- DFS,BFS/BOJ_7576_토마토.py
+++ DFS,BFS/BOJ_7576_토마토.py
@@ -1,37 +1,40 @@
 from collections import deque
+import sys
+input=sys.stdin.readline
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def bfs():
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny]==0:
+                    q.append((nx,ny))
+                    graph[nx][ny]=graph[x][y]+1
+
 
 m,n=map(int,input().split())
-
 graph=[]
-for i in range(n):
+for _ in range(n):
     graph.append(list(map(int,input().split())))
-queue=deque([])
 
-dx,dy=[-1,1,0,0],[0,0,-1,1]
-
-result=0
-
+q=deque([])
 for i in range(n):
     for j in range(m):
         if graph[i][j]==1:
-            queue.append([i,j])
-
-def bfs():
-    while queue:
-        x,y=queue.popleft()
-        for i in range(4):
-            nx=dx[i]+x
-            ny=dy[i]+y
-            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
-                graph[nx][ny]=graph[x][y]+1
-                queue.append([nx,ny])
+            q.append((i,j))
 
 bfs()
+result=0
+
 for i in graph:
-    for j in i:
-        if j==0:
-            print(-1)
-            exit(0)
+    if 0 in i:
+        print(-1)
+        exit()
     result=max(result,max(i))
 
 print(result-1)
