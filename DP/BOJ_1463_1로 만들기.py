d9f55cea40a9e5bd679f43ea9b7b4543717a41c1
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-05-18 16:58:14 +0900

    Update BOJ_1463_1로 만들기.py

--- DP/BOJ_1463_1로 만들기.py
+++ DP/BOJ_1463_1로 만들기.py
@@ -1,10 +1,12 @@
 n=int(input())
-d=[0]*(n+1)
-
-d[0]=0
-d[1]=1
+dp=[0]*(n+1)
 
 for i in range(2,n+1):
-    d[i]=d[i-1]+d[i-2]
+    dp[i]=dp[i-1]+1
+    
+    if i%3==0:
+        dp[i]=min(dp[i],dp[i//3]+1)
+    if i%2==0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
 
-print(d[n])
+print(dp[n])
