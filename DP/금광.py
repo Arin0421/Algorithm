d6528113efb6ad6da4fcb7cfcfd307b825a5d0ba
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-06-14 16:44:14 +0900

    Update 금광.py

--- DP/금광.py
+++ DP/금광.py
@@ -5,26 +5,23 @@ for _ in range(t):
     arr=list(map(int,input().split()))
 
     dp=[]
-    index=0
-    for i in range(m):
-        dp.append(arr[index:index+m])
-        index+=m
-
-    for j in range(1,m):
-        for i in range(n):
-            if i==0:
-                left_up=0
-            else:
-                left_up=dp[i-1][j-1]
-            if i==n-1:
-                left_down=0
-            else:
-                left_down=dp[i+1][j-1]
-            left=dp[i][j-1]
-            dp[i][j]=dp[i][j]+max(left_up,left_down,left)
-
-    result=0
+    idx=0
     for i in range(n):
-        result=max(result,dp[i][m-1])
+        dp.append(arr[idx:idx+m])
+        idx+=m
 
-    print(result)
+    for i in range(1,m):
+        for j in range(n):
+            if j==0:
+                dp[j][i]+=max(dp[j][i-1],dp[j+1][i-1])
+            elif j==1:
+                dp[j][i]+=max(dp[j-1][i-1],dp[j][i-1],dp[j+1][i-1])
+            else:
+                dp[j][i]+=max(dp[j-1][i-1],dp[j][i-1])
+
+    ans=0
+    for i in range(n):
+        if ans<dp[i][m-1]:
+            ans=dp[i][m-1]
+
+    print(ans)
