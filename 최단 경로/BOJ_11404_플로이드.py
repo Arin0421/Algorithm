06744828a12c0b59f171718bbedef306bacf176f
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-06-30 16:24:22 +0900

    Update BOJ_11404_플로이드.py

--- 최단 경로/BOJ_11404_플로이드.py
+++ 최단 경로/BOJ_11404_플로이드.py
@@ -1,20 +1,19 @@
 import sys
 input=sys.stdin.readline
-INF = int(1e9)
+INF=int(1e9)
+
 n=int(input())
 m=int(input())
-
-graph=[ [INF]*(n+1) for _ in range(n+1)]
+graph=[[INF]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a,b,c=map(int,input().split())
+    if graph[a][b]>c:
+        graph[a][b]=c
 
 for a in range(1,n+1):
     for b in range(1,n+1):
         if a==b:
             graph[a][b]=0
-for _ in range(m):
-    a,b,c=map(int,input().split())
-    if graph[a][b]>c:
-        graph[a][b]=c
-    
 
 for k in range(1,n+1):
     for a in range(1,n+1):
