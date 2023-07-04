d10c84f45ff8c40e56846ce8ab6561b9367260fc
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-07-04 15:49:14 +0900

    Update 숨바꼭질.py

--- 최단 경로/숨바꼭질.py
+++ 최단 경로/숨바꼭질.py
@@ -1,33 +1,41 @@
 import sys
+import heapq
 input=sys.stdin.readline
 INF=int(1e9)
 
 n,m=map(int,input().split())
-graph=[ [INF]*(n+1) for _ in range(n+1)]
-
+graph=[ [] for _ in range(n+1)]
 for _ in range(m):
     a,b=map(int,input().split())
-    graph[a][b]=1
-    graph[b][a]=1
+    graph[a].append((b,1))
+    graph[b].append((a,1))
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==j:
-            graph[i][j]=0
+start=1
+distance=[INF]*(n+1)
+def dijkstra(start):
+    q=[]
+    heapq.heappush(q,(0,start))
+    distance[start]=0
+    while q:
+        dist,now=heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q,(cost,i[0]))
 
-for k in range(1,n+1):
-    for a in range(1,n+1):
-        for b in range(1,n+1):
-            graph[a][b]=min(graph[a][b],graph[a][k]+graph[k][j])
+dijkstra(start)
 
-ans=[]
-m_val=max(graph[1][1:])
+max_distance=max(distance[1:])
 cnt=0
+ans=[]
 for i in range(1,n+1):
-    if graph[1][i]==m_val:
+    if distance[i]==max_distance:
         ans.append(i)
         break
-ans.append(m_val)
-ans.append(graph[1].count(m_val))
+ans.append(max_distance)
+ans.append(distance.count(max_distance))
 
 print(*ans)
