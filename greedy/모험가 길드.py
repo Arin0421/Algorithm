9eb3ca3a854e0198da6b4a595d3a76872dbffa03
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-07-07 15:31:36 +0900

    Update 모험가 길드.py

--- greedy/모험가 길드.py
+++ greedy/모험가 길드.py
@@ -1,14 +1,13 @@
-n=map(int,input())
-data = list(map(int,input().split()))
+n=int(input())
+data=list(map(int,input().split()))
 data.sort()
 
 result=0
-count=0 #그룹에 속한 모험가의 수를 나타내기 위해 사
+cnt=0
 for i in data:
-    count+=1
-    if count>=i: #현재 그룹에 속한 모험가의 수가 현재 공포도 이상이라면 그룹 결성
+    cnt+=1
+    if cnt>=i:
         result+=1
-        count=0 #그룹에 속한 모험가의 수 초기화
+        cnt=0
 
 print(result)
-
