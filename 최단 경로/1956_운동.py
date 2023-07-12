085a46f27658419e7d5c5c6000814e4a19c44972
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-07-12 19:16:47 +0900

    Create 1956_운동.py

---
+++ 최단 경로/1956_운동.py
@@ -0,0 +1,24 @@
+import sys
+input=sys.stdin.readline
+INF=int(1e9)
+
+v,e=map(int,input().split())
+graph=[[INF]*(v+1) for _ in range(v+1)]
+
+for _ in range(e):
+    a,b,c=map(int,input().split())
+    graph[a][b]=c
+
+for k in range(1,v+1):
+    for i in range(1,v+1):
+        for j in range(1,v+1):
+            graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
+
+ans=1e9
+for i in range(1,v+1):
+    for j in range(1,v+1):
+        if i==j:
+            if ans>graph[i][j]:
+                ans=graph[i][j]
+
+print(ans if ans!=INF else -1)
