900289e266a2670abdbb95f35a9d8da79c9514a9
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-07-13 17:45:20 +0900

    Update 다음 큰 숫자.py

--- 시뮬레이션과 구현/다음 큰 숫자.py
+++ 시뮬레이션과 구현/다음 큰 숫자.py
@@ -1,21 +1,9 @@
-def cnt_one(x):
-    cnt=0
-    for i in x:
-        if i=='1':
-            cnt+=1
-    return cnt
-
 def solution(n):
-    answer = n
-    while True:
-        answer+=1
+    num=bin(n).count('1')
     
-        n_temp=bin(n)[2:]
-        ans_temp=bin(answer)[2:]
-        
-        n_one=cnt_one(n_temp)
-        ans_one=cnt_one(ans_temp)
-        
-        if n_one==ans_one:
+    while True:
+        n+=1
+        temp=bin(n).count('1')
+        if num==temp:
             break
-    return answer
+    return n
