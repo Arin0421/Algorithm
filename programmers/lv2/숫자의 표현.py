b62919ea6f18d98fb3301fb9511eb78888612964
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-07-17 20:56:23 +0900

    Update 숫자의 표현.py

--- programmers/lv2/숫자의 표현.py
+++ programmers/lv2/숫자의 표현.py
@@ -2,12 +2,11 @@ def solution(n):
     answer = 0
     for i in range(1,n+1):
         temp=0
-        temp+=i
-        for j in range(i+1,n+1):
+        for j in range(i,n+1):
             temp+=j
             if temp==n:
                 answer+=1
                 break
             elif temp>n:
                     break
-    return answer+1
+    return answer
