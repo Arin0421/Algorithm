0ae2232fb74f4cd0378178aaa1c7d83918033b31
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-07-28 14:37:42 +0900

    Create k진수에서 소수 개수 구하기.py

---
+++ programmers/lv2/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,21 @@
+def solution(n, k):
+    word=""
+    while n:
+        word = str(n%k)+word
+        n=n//k
+        
+    word=word.split("0")
+    
+    answer=0
+    for w in word:
+        if len(w)==0 or int(w)<2:
+            continue
+        is_prime=True
+        for i in range(2,int(int(w)**0.5)+1): 
+            if int(w)%i==0:
+                is_prime=False
+                break
+        if is_prime:
+            answer+=1
+            
+    return answer
