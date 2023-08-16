f75f89ac6661c0ed191131263d0e32ca43b14670
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-08-16 17:26:30 +0900

    Update 15686_치킨 배달.py

--- 삼성 SW 역량 기출/15686_치킨 배달.py
+++ 삼성 SW 역량 기출/15686_치킨 배달.py
@@ -1,31 +1,26 @@
-import sys
 from itertools import combinations
-input=sys.stdin.readline
-
 n,m=map(int,input().split())
 graph=[list(map(int,input().split())) for _ in range(n)]
 
-chi=[]
-house=[]
+def chik_distance(hx,hy,cx,cy):
+    return abs(hx-cx)+abs(hy-cy)
+
+c=[]
+p=[]
 for i in range(n):
     for j in range(n):
         if graph[i][j]==2:
-            chi.append((i,j))
-        elif graph[i][j]==1:
-            house.append((i,j))
-
-def dis(x,y,a,b):
-    res=abs(x-a)+abs(y-b)
-    return res
-
+            c.append((i,j))
+        if graph[i][j]==1:
+            p.append((i,j))
 ans=1e9
-for c in combinations(chi,m):
-    total=0
-    for x,y in house:
+for chick in list(combinations(c,m)):
+    temp=0
+    for px,py in p:
         distance=1e9
-        for a,b in c:
-            distance=min(distance,dis(x,y,a,b))
-        total+=distance
-    ans=min(ans,total)
+        for cx,cy in chick:
+            distance=min(distance,chik_distance(px,py,cx,cy))
+        temp+=distance
+    ans=min(temp,ans)
 
 print(ans)
