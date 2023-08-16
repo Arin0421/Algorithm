abd354ce8dceca3529830337764e3093e012ce75
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-08-16 16:37:00 +0900

    Update 문자열 압축.py

--- 시뮬레이션과 구현/문자열 압축.py
+++ 시뮬레이션과 구현/문자열 압축.py
@@ -1,17 +1,16 @@
 def solution(s):
     answer = len(s)
-    for step in range(1,len(s)//2+1):
+    for step in range(1,(len(s)//2)+1):
         compressed=""
         prev=s[0:step]
-        count=1
-
-        for j in range(step, len(s), step):
-            if prev == s[j:j+step]:
-                count+=1
+        cnt=1
+        for i in range(step,len(s),step):
+            if prev==s[i:i+step]:
+                cnt+=1
             else:
-                compressed += str(count)+prev if count>=2 else prev
-                prev = s[j:j+step]
-                count=1
-        compressed += str(count)+prev if count>=2 else prev
-        answer = min(answer, len(compressed))
-    return anser
+                compressed+=str(cnt)+prev if cnt>=2 else prev
+                prev = s[i:i+step]
+                cnt=1
+        compressed+=str(cnt)+prev if cnt>=2 else prev
+        answer = min(answer,len(compressed))
+    return answer
