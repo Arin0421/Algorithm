f82b04fcb25c99b15d75d704ec5a9db5ee4fc41b
ARIN LEE <73944423+Arin0421@users.noreply.github.com>
2023-08-17 22:00:21 +0900

    Update 실패율.py

--- 정렬/실패율.py
+++ 정렬/실패율.py
@@ -1,17 +1,19 @@
-def solution(n, stages):
+def solution(N, stages):
     answer = []
-    now=[0]*(n+2)
+    now=[0]*(N+2)
+    
     for i in stages:
         now[i]+=1
     
-    for i in range(1,n+1):
+    for i in range(1,N+1):
         if now[i]==0:
             answer.append((i,0))
         else:
             answer.append((i,now[i]/sum(now[i:])))
+    
     answer=sorted(answer,key=lambda x:-x[1])
     
-    for i in range(n):
+    for i in range(N):
         answer[i]=answer[i][0]
-        
+    
     return answer
